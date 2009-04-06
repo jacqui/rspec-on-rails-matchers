10782415144d283d921354fdfedfed69ada65def
Matt Patterson <matt@reprocessed.org>
2009-04-06 11:53:17 +0100

    Trying to get better spec coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,14 @@
+RAILS_ENV = 'test'
 begin
   require 'rubygems'
+  require 'activesupport'
   require 'spec'
+  gem 'rspec-rails'
+  require 'spec/rails'
   
 rescue LoadError
-  puts "Please install rspec and mocha to run the tests."
+  puts $!.inspect
+  puts "Please install rspec, rspec-rails and mocha to run the tests."
   exit 1
 end
 
